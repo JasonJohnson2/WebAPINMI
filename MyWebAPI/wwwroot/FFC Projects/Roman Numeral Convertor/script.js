ef740365d55de799e9e9e65f6fb28aab50a36ccf
JasonJohnson2 <96836886+JasonJohnson2@users.noreply.github.com>
2026-06-09 10:40:49 -0400

    Delete MyWebAPI directory

--- MyWebAPI/wwwroot/FFC Projects/Roman Numeral Convertor/script.js
+++
@@ -1,8 +0,0 @@
-const input = document.getElementById("number");
-const output = document.getElementById("output");
-const button = document.getElementById("convert-btn");
-
-button.addEventListener("click", () => {
-  const number = input.value;
-  output.innerHTML = convertToRoman(number);
-});
